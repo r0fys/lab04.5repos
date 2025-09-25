162506aa0b576c18d094d5e9ece55cb464ee999c
r0fys <rofysyt@gmail.com>
2025-09-25 11:11:19 +0300

    f2

--- PR_4.5/PR_4.5.cpp
+++ PR_4.5/PR_4.5.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    double x, y, R;
+    int x, y, R, random_x, random_y;
 
     /*1 спосіб: Для перших десяти пострілів, координати (x, y) яких вводяться
     з клавіатури, виводити текстові повідомлення про попадання в мішень.*/
@@ -30,15 +30,15 @@ int main()
     cout << endl << "R = "; cin >> R; // питання: "чи треба"? 
     for (int i = 0; i < 10; i++)
     {
-        cout << i + 1 << ") x = "; cin >> x;
-        cout << i + 1 << ") y = "; cin >> y;
+        random_x = -R + rand() % (R + 1) * 2;
+        random_y = -R + rand() % (R + 1) * 2;
 
-        if (x <= R && x >= -R && y <= R && y >= -R)
-        {
-            if ((pow(x, 2) + pow(y, 2) <= pow(R, 2)) && ((x >= 0 && y >= 0 && y >= x) || (x <= 0 && y <= 0 && y <= x)))
-                cout << endl << "hit 2" << endl;
-            else  cout << endl << "no 2" << endl;
-        }
+        cout << i + 1 << ") x = " << random_x;
+        cout << "  |  " << "y = " << random_y;
+
+        if ((pow(random_x, 2) + pow(random_y, 2) <= pow(R, 2)) && ((random_x >= 0 && random_y >= 0 && random_y >= random_x) || (random_x <= 0 && random_y <= 0 && random_y <= random_x)))
+            cout << endl << "hit 1" << endl;
+        else  cout << endl << "no 1" << endl;
     }
     return 0;
 }
