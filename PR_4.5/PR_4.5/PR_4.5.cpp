fe837125f35df3bb31ca980cdb6ef12225e71da5
r0fys <rofysyt@gmail.com>
2025-09-28 15:29:02 +0300

    f3

--- PR_4.5/PR_4.5/PR_4.5.cpp
+++ PR_4.5/PR_4.5/PR_4.5.cpp
@@ -1,44 +1,37 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
 
 int main()
 {
-    double x, y, R;
-
-    /*1 спосіб: Для перших десяти пострілів, координати (x, y) яких вводяться 
-    з клавіатури, виводити текстові повідомлення про попадання в мішень.*/
+    int x, y, R, random_x, random_y;
 
     cout << "R = "; cin >> R;
 
-    for (int i = 0; i < 10; i++) 
-    {
-        cout << i+1 <<") x = "; cin >> x;
-        cout << i+1 <<") y = "; cin >> y;
-
-        if ((pow(x, 2) + pow(y, 2) <= pow(R, 2)) && ((x >= 0 && y >= 0 && y >= x) || (x <= 0 && y <= 0 && y <= x)))
-            cout << endl << "hit 1" << endl;
-        else  cout << endl << "no 1" << endl;
-    }
-
-    x, y, R = 0;
-
-    /*2 спосіб: Для наступних десяти пострілів, координати (x, y) 
-    яких визначаються випадково із вказаного інтервалу x, y[A; B], 
-    виводити значення координат (x, y) та текстові повідомлення про попадання в мішень.*/
-
-    cout <<endl<< "R = "; cin >> R; // питання: "чи треба"? 
     for (int i = 0; i < 10; i++)
     {
-        cout << i+1 << ") x = "; cin >> x;
-        cout << i+1 << ") y = "; cin >> y;
+        cout << i + 1 << ") x = "; cin >> x;
+        cout << i + 1 << ") y = "; cin >> y;
 
-        if (x <= R && x >= -R && y <= R && y >= -R)
-        {
-            if ((pow(x, 2) + pow(y, 2) <= pow(R, 2)) && ((x >= 0 && y >= 0 && y >= x) || (x <= 0 && y <= 0 && y <= x)))
-                cout << endl << "hit 2" << endl;
-            else  cout << endl << "no 2" << endl;
-        }
+        if ((pow(x, 2) + pow(y, 2) <= pow(R, 2)) && ((x >= 0 && y >= 0 && y >= x) || (x <= 0 && y <= 0 && y <= x)))
+            cout << "hit 1" << endl;
+        else  cout << "no 1" << endl;
+    }
+
+    x, y = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        random_x = -R + rand() % (R + 1) * 2;
+        random_y = -R + rand() % (R + 1) * 2;
+
+        cout << setw(2) << i + 1 << ") x = " << random_x;
+        cout << "  |  " << "y = " << random_y;
+
+        if ((pow(random_x, 2) + pow(random_y, 2) <= pow(R, 2)) && ((random_x >= 0 && random_y >= 0 && random_y >= random_x) || (random_x <= 0 && random_y <= 0 && random_y <= random_x)))
+            cout << endl << "hit 1" << endl;
+        else  cout << endl << "no 1" << endl;
     }
     return 0;
 }
