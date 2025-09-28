99c77dda866a9246d438be59478bbe4b9cf21a7b
r0fys <rofysyt@gmail.com>
2025-09-28 16:48:33 +0300

    f4

--- PR_4.5/PR_4.5/PR_4.5.cpp
+++ PR_4.5/PR_4.5/PR_4.5.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    int x, y, R, random_x, random_y;
+    int x, y, R;
 
     cout << "R = "; cin >> R;
 
@@ -15,23 +15,23 @@ int main()
         cout << i + 1 << ") y = "; cin >> y;
 
         if ((pow(x, 2) + pow(y, 2) <= pow(R, 2)) && ((x >= 0 && y >= 0 && y >= x) || (x <= 0 && y <= 0 && y <= x)))
-            cout << "hit 1" << endl;
-        else  cout << "no 1" << endl;
+            cout << "hit" << endl;
+        else  cout << "no" << endl;
     }
 
     x, y = 0;
 
     for (int i = 0; i < 10; i++)
     {
-        random_x = -R + rand() % (R + 1) * 2;
-        random_y = -R + rand() % (R + 1) * 2;
+        x = -R + rand() % (R + 1) * 2;
+        y = -R + rand() % (R + 1) * 2;
 
-        cout << setw(2) << i + 1 << ") x = " << random_x;
-        cout << "  |  " << "y = " << random_y;
+        cout << setw(2) << i + 1 << ") x = " << setw(2) <<x;
+        cout << "  |  " << "y = " << setw(2) << y;
 
-        if ((pow(random_x, 2) + pow(random_y, 2) <= pow(R, 2)) && ((random_x >= 0 && random_y >= 0 && random_y >= random_x) || (random_x <= 0 && random_y <= 0 && random_y <= random_x)))
-            cout << endl << "hit 1" << endl;
-        else  cout << endl << "no 1" << endl;
+        if ((pow(x, 2) + pow(y, 2) <= pow(R, 2)) && ((x >= 0 && y >= 0 && y >= x) || (x <= 0 && y <= 0 && y <= x)))
+            cout << endl << "hit" << endl;
+        else  cout << endl << "no" << endl;
     }
     return 0;
 }
